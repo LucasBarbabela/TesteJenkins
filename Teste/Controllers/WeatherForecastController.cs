a0286bab78e44b4a4c5e243369d212c6fb0d0615
Lucas Vilela <lucasvileladev@gmail.com>
2020-09-30 15:53:47 -0300

    Testando o webhook

--- Teste/Controllers/WeatherForecastController.cs
+++ Teste/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace Teste.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "teste", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
