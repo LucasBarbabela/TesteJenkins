576f1d02deb8ef1d155b5b35456f7b82d9071e5f
Lucas Vilela <lucasvileladev@gmail.com>
2020-09-30 15:19:22 -0300

    Forçando erros

--- Teste/Program.cs
+++ Teste/Program.cs
@@ -16,11 +16,5 @@ namespace Teste
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
     }
 }
